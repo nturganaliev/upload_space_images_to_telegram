cd1d9fecbf7500019154cb2072dd71cf26c45b4b
Nurlan Turganaliev <nurlan.turganaliev@gmail.com>
2023-01-04 16:45:02 +0600

    Publish photo to Telegram channel

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ def main():
     updates = bot.get_updates()
     chat_id = updates[-1].message.chat_id
 
-    bot.send_message(text="Hi Nurlan", chat_id=chat_id)
-    bot.send_message(text="This is another test message", chat_id=chat_id)
-    bot.send_message(text="I'm sorry Nurlan I'm afraid I can't do that.", chat_id=chat_id)
+    # bot.send_message(text="Hi Nurlan", chat_id=chat_id)
+    # bot.send_message(text="This is another test message", chat_id=chat_id)
+    # bot.send_message(text="I'm sorry Nurlan I'm afraid I can't do that.", chat_id=chat_id)
+
+    bot.send_document(chat_id=chat_id, document=open("images/nasa_2.jpg", "rb"))
 
 
 if __name__ == '__main__':
